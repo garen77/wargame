5ba9142a0acd4fa59834e318b6a1f68fabd0830b
garen <garen@f00257e0-c555-49c8-8fe5-05fec8127eca>
2014-10-03 20:16:19 +0000

    git-svn-id: https://subversion.assembla.com/svn/war-game-repo/trunk@5 f00257e0-c555-49c8-8fe5-05fec8127eca

---
+++ WarGameSharedLibraries/db/insertCharacterFeature01.sql
@@ -0,0 +1,25 @@
+﻿INSERT INTO "Feature"(id, des)VALUES (10, 'Knife');
+INSERT INTO "Feature"(id, des)VALUES (20, 'Gun');
+INSERT INTO "Feature"(id, des)VALUES (30, 'Rifle');
+INSERT INTO "Feature"(id, des)VALUES (40, 'Machine Gun');
+INSERT INTO "Feature"(id, des)VALUES (50, 'Bazooka');
+
+insert into "CharacterFeature" ("idCharacter","idFeature")values(10,10);
+
+insert into "CharacterFeature" ("idCharacter","idFeature")values(20,10);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(20,20);
+
+insert into "CharacterFeature" ("idCharacter","idFeature")values(30,10);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(30,20);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(30,30);
+
+insert into "CharacterFeature" ("idCharacter","idFeature")values(40,10);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(40,20);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(40,30);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(40,40);
+
+insert into "CharacterFeature" ("idCharacter","idFeature")values(50,10);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(50,20);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(50,30);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(50,40);
+insert into "CharacterFeature" ("idCharacter","idFeature")values(50,50);
