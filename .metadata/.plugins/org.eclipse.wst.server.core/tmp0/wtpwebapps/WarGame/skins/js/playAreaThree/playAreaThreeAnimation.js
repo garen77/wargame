90826b355ad88ce31802195bb1a4d004363a79a9
garen <garen@f00257e0-c555-49c8-8fe5-05fec8127eca>
2014-10-14 22:02:08 +0000

    added dat.GUI management
    git-svn-id: https://subversion.assembla.com/svn/war-game-repo/trunk@20 f00257e0-c555-49c8-8fe5-05fec8127eca

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/WarGame/skins/js/playAreaThree/playAreaThreeAnimation.js
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/WarGame/skins/js/playAreaThree/playAreaThreeAnimation.js
@@ -5,8 +5,15 @@ var scene = null,
 	cube = null,
 	camera = null,
 	sphere = null,
-	step=0;
+	step=0,
+	controls = new function() {
+		this.rotationSpeed = 0.02;
+		this.bouncingSpeed = 0.03;
+		};
 
+var gui = new dat.GUI();
+		gui.add(controls, 'rotationSpeed',0,0.5);
+		gui.add(controls, 'bouncingSpeed',0,0.5);
 $(function () {
 	
 	
@@ -77,11 +84,11 @@ function initStats() {
 
 function renderScene()
 {
-	cube.rotation.x += 0.02;
-	cube.rotation.y += 0.02;
-	cube.rotation.z += 0.02;
+	cube.rotation.x += controls.rotationSpeed;
+	cube.rotation.y += controls.rotationSpeed;
+	cube.rotation.z += controls.rotationSpeed;
 	
-	step+=0.04;
+	step+=controls.bouncingSpeed;
 	sphere.position.x = 20+( 10*(Math.cos(step)));
 	sphere.position.y = 2 +( 10*Math.abs(Math.sin(step)));
 	
